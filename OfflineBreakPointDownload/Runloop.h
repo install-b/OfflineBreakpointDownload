6bd528bdd4b2658586bace8be478f17d9ae22eae
Shangen Zhang <shangenzhang@shangendemacbook-pro.local>
2017-01-14 12:34:51 +0800

    add runloop

---
+++ OfflineBreakPointDownload/Runloop.h
@@ -0,0 +1,13 @@
+//
+//  Runloop.h
+//  OfflineBreakPointDownload
+//
+//  Created by Shangen Zhang on 17/1/14.
+//  Copyright © 2017年 Shangen Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Runloop : NSObject
+
+@end
